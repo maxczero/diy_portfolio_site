07a4c0f65dd131f4798230bc9b38e33a04176c50
Max Chidzero <mwc349@nyu.edu>
2021-03-25 18:57:45 +0100

    minor additions towards markdown articles

--- src/scripts/article_previewer.js
+++ src/scripts/article_previewer.js
@@ -1,1 +1,7 @@
 // TODO: add markdown it parser.
+var MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
+var result = md.render(URL);
+
+
+const post = document.getElementsByClassName("post");
